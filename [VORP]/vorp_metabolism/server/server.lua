516326e37737630bbf5b6a106b32a4cac611fd3e
cp13377 <81640829+cp13377@users.noreply.github.com>
2024-08-04 16:34:55 +0200

    Added logging for item usage and status updates to vorp_metabolism server script.

--- [VORP]/vorp_metabolism/server/server.lua
+++ [VORP]/vorp_metabolism/server/server.lua
@@ -11,18 +11,20 @@ RegisterNetEvent("vorpmetabolism:SaveLastStatus", function(status)
     local _source = source
     local UserCharacter = VorpCore.getUser(_source).getUsedCharacter
     UserCharacter.setStatus(status)
+    print("Status saved: " .. status)
 end)
 RegisterNetEvent("vorpmetabolism:GetStatus", function()
     local _source = source
     local UserCharacter = VorpCore.getUser(_source).getUsedCharacter
     local s_status = UserCharacter.status
+    print("Status retrieved: " .. s_status)
     if (#s_status > 5) then
         TriggerClientEvent("vorpmetabolism:StartFunctions", _source, s_status)
     else
         local status = json.encode({
             ['Hunger'] = Config["FirstHungerStatus"],
             ['Thirst'] = Config["FirstThirstStatus"],
-            ['Metabolism'] = Config["FirstMetabolismStatus"]
+            ['Metabolism"] = Config["FirstMetabolismStatus"]
         })
         UserCharacter.setStatus(status)
         TriggerClientEvent("vorpmetabolism:StartFunctions", _source, status)
@@ -43,6 +45,8 @@ function RegisterUsableItemsAsync()
             local itemLabel = data.item.label
             TriggerClientEvent("vorpmetabolism:useItem", data.source, i, itemLabel)
             TriggerEvent("vorpCore:subItem", data.source, Config["ItemsToUse"][i]["Name"], 1)
+            print("Item used: " .. itemLabel)
         end)
     end
 end
+
